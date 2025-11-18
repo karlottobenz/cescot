1d2eecf09cd4a398c49f8333ca0016f03944e64e
karlottobenz <fmsemprini@gmail.com>
2025-11-18 17:23:43 +0100

    debug

---
+++ crossi/php/info.php
@@ -0,0 +1,21 @@
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Esercizio PHP</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World";
+
+        $variable = 42;
+        echo "The answer is $variable";
+
+        if($variable > 40) {
+            echo "The variable is greater than 40";
+        } else{
+            echo "The variable is 40 or less";
+        }
+        ?>
+    </body>
+</html>
