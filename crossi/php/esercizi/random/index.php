80b28f6bffe8087a657c7499ac390af1c0050bc0
karlottobenz <fmsemprini@gmail.com>
2025-11-24 17:05:06 +0100

    .

---
+++ crossi/php/esercizi/random/index.php
@@ -0,0 +1,17 @@
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Esercizio</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+        <body>
+            <?php
+                for ($i = 1; $i <= 10; $i++){
+                    echo "<div>Questo è un div pari</div>";
+                    echo "<div>Questo è un div dispari</div>";
+                }
+            ?>
+        </body>
+    </head>
+</html>
