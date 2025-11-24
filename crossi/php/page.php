866dcd962193f77c700616bf54fa5b8ea256c35c
karlottobenz <fmsemprini@gmail.com>
2025-11-24 16:06:41 +0100

    Creato page.php

---
+++ crossi/php/page.php
@@ -0,0 +1,28 @@
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Esercizio</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+        <body>
+            <h1>Pagina di prova</h1>
+            <div class="div-num">Questo è il div 1</div>
+            <div class="div-num">Questo è il div 2</div>
+            <div class="div-num">Questo è il div 3</div>
+            <div class="div-num">Questo è il div 4</div>
+            <div class="div-num">Questo è il div 5</div>
+            <div class="div-num">Questo è il div 6</div>
+            <div class="div-num">Questo è il div 7</div>
+            <div class="div-num">Questo è il div 8</div>
+            <div class="div-num">Questo è il div 9</div>
+            <div class="div-num">Questo è il div 10</div>
+            <div class="div-num">Questo è il div 11</div>
+            <div class="div-num">Questo è il div 12</div>
+            <div class="div-num">Questo è il div 13</div>
+            <div class="div-num">Questo è il div 14</div>
+            <div class="div-num">Questo è il div 15</div>
+            <div class="div-foot">Questa è la fine della pagina</div>
+        </body>
+    </head>
+</html>
