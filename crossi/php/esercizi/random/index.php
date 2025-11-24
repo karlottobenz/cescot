629bbcd554c8818b71c322de8bf07969b637b653
karlottobenz <fmsemprini@gmail.com>
2025-11-24 17:25:04 +0100

    finale

--- crossi/php/esercizi/random/index.php
+++ crossi/php/esercizi/random/index.php
@@ -4,12 +4,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Esercizio</title>
         <?php
-        $random = RAND(1, 3);
+        $random = RAND(1, 3); echo $random;
         if($random == 1){
             echo '<link href="style_1.css" rel="stylesheet">';
         } elseif($random == 2){
             echo '<link href="style_2.css" rel="stylesheet">';
-        } elseif($random == 3){
+        } else {
             echo '<link href="style_3.css" rel="stylesheet">';
         }
         ?>
@@ -18,8 +18,8 @@
             <h1>CSS casuali</h1>
             <?php
                 for ($i = 1; $i <= 10; $i++){
-                    echo "<div>Questo è un div pari</div>";
-                    echo "<div>Questo è un div dispari</div>";
+                    echo "<div class='divpari'>Questo è un div pari</div>";
+                    echo "<div class='divdispari'>Questo è un div dispari</div>";
                 }
             ?>
             <div>Questa è la fine della pagina</div>
