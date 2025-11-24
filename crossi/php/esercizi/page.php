cba32a02748963a5b79b811e5537d86103245569
MiSKuAndre <andreabrighi0902@gmail.com>
2025-11-24 16:48:14 +0100

    php

---
+++ crossi/php/esercizi/page.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>pagina di prova</h1>
+    <?php
+    for ($i = 1; $i <= 10; $i++){
+        echo "<p>Questo è il paragrafo numero $i</p>";
+    }
+    ?>
+    
+</body>
+</html>
