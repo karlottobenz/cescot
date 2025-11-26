764c9a8e9b1b98e5594f57ab2a2f0b2e3861df68
karlottobenz <fmsemprini@gmail.com>
2025-11-26 15:55:19 +0100

    random2

--- crossi/php/esercizi/random2/index.php
+++ crossi/php/esercizi/random2/index.php
@@ -1,3 +1,19 @@
 <html>
-    
+    <head>
+        <title>Esercizio</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1> Contenuti a caso!</h1>
+        <?php
+            $n = RAND(10, 30);
+            for($i = 1; $i <= $n; $i++){
+                if($i % 2 != 0) {
+                    echo "<div class='dispari'>io sono dispari</div>";
+                } else {
+                    echo "<div class='pari'>io sono pari</div>";
+                }
+            }
+        ?>
+    </body>
 </html>
