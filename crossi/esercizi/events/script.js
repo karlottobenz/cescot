f2d4af3640c2364d7be0fe773a99e93456c88757
karlottobenz <fmsemprini@gmail.com>
2025-12-03 16:26:56 +0100

    Bottone colora

--- crossi/esercizi/events/script.js
+++ crossi/esercizi/events/script.js
@@ -1,6 +1,8 @@
-var bottonePremuto = document.getElementById("bottone");
-var div = document.getElementById("div");
+var bottonePremuto = document.getElementById('bottone');
+var div = document.getElementsByClassName("div");
 
-bottonePremuto.addEventListener("click", function(){
-    div.style = background-color: red;
+bottonePremuto.addEventListener('click', function(){
+    for(let i = 0; i <= 19; i++){
+        div[i].style = "background-color: red";
+    }
 })
