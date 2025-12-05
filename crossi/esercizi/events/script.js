20205d255c28e43fa3af5ae85c99372bdff2a2d6
karlottobenz <fmsemprini@gmail.com>
2025-12-05 11:26:14 +0100

    aggiunto bottone blu

--- crossi/esercizi/events/script.js
+++ crossi/esercizi/events/script.js
@@ -1,15 +1,28 @@
 var bottoneRosso = document.getElementById('bottonerosso');
 var bottoneGiallo = document.getElementById('bottonegiallo');
+var bottoneBlu = document.getElementById('bottoneblu');
 var div = document.getElementsByClassName("div");
 
 bottoneRosso.addEventListener('click', function(){
     for(let i = 0; i <= 19; i++){
-        div[i].classList.add("red");
+        div[i].classList.remove("giallo");
+        div[i].classList.remove("blu");
+        div[i].classList.add("rosso");
     }
 })
 
 bottoneGiallo.addEventListener('click', function(){
     for(let i = 0; i <= 19; i++){
-        div[i].classList.remove("red");
+        div[i].classList.remove("rosso");
+        div[i].classList.remove("blu");
+        div[i].classList.add("giallo");
+    }
+})
+
+bottoneBlu.addEventListener('click', function(){
+    for(let i = 0; i <= 19; i++){
+        div[i].classList.remove("rosso");
+        div[i].classList.remove("giallo");
+        div[i].classList.add("blu");
     }
 })
