337a46836190da839f5e7a057b19abd394ebfe9c
karlottobenz <fmsemprini@gmail.com>
2025-12-15 12:05:57 +0100

    include

--- crossi/esercizi/groups/pagina.php
+++ crossi/esercizi/groups/pagina.php
@@ -1,19 +1,32 @@
-<html lang="en">
+<?php
+    include('../../php/esercizi/lib/libreria.php')
+?>
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Esercizio</title>
 </head>
 <link rel ="stylesheet" href =style.css>
 <body>
     <div>
-        <button id="bottonerosso">Rosso</button> <br>
-        <button id="bottoneblu">Blu</button> <br>
+        <button id="primorosso">Primo rosso</button>
+        <button id="secondorosso">Secondo rosso</button>
+        <button id="tuttirosso">Tutti rosso</button>
+        <br>
+        <button id="primoblu">Primo blu</button>
+        <button id="secondoblu">Secondo blu</button>
+        <button id="tuttiblu">Tutti blu</button>
+        <br>
     </div>
+    <h2>Primo Gruppo</h2>
      <?php
-    for ($i = 1; $i <= 15; $i++){
-        echo("<div>Questo è il div numero $i</div>");
-    }
+        rand_div_primo();
+    ?>
+
+    <h2>Secondo Gruppo</h2>
+     <?php
+        rand_div_secondo();
     ?>
     
 </body>
