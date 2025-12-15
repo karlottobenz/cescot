fe4063973db6ae6076c7974065c26bdcc41d7491
lisa <lisa.casaburo@outlook.it>
2025-12-15 11:58:54 +0100

    ....

--- crossi/esercizi/groups/pagina.php
+++ crossi/esercizi/groups/pagina.php
@@ -4,8 +4,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link rel ="stylesheet" href =style.css>
 <body>
-    <button id="bottonerosso">Rosso</button>
+    <div>
+        <button id="bottonerosso">Rosso</button> <br>
+        <button id="bottoneblu">Blu</button> <br>
+    </div>
      <?php
     for ($i = 1; $i <= 15; $i++){
         echo("<div>Questo è il div numero $i</div>");
