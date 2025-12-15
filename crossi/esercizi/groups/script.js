10268ae2cb45676b5daf4cd7ce0029616f00eb78
lisa <lisa.casaburo@outlook.it>
2025-12-15 12:23:09 +0100

    colori

--- crossi/esercizi/groups/script.js
+++ crossi/esercizi/groups/script.js
@@ -1,1 +1,22 @@
+var bottoneRosso = document.getElementById('Primorosso');
+var bottoneprimo_gruppo = document.getElementById('Secondorosso');
+var bottonesecondo_gruppo = document.getElementById('Tuttirosso');
+var bottoneblu = document.getElementById('Primoblu');
+var bottoneprimo_gruppo = document.getElementById('Secondoblu');
+var bottonesecondo_gruppo = document.getElementById('Tuttiblu');
 
+PrimoRosso.addEventListener('click', function(){
+    for(let i = 0; rand; i++){
+        div[i].classList.add("rosso");
+        div[i].classList.add("primo_gruppo");
+        div[i].classList.remove("blu");
+    }
+})
+
+SecondoRosso.addEventListener('click', function(){
+    for(let i = 0; i <= 19; i++){
+        div[i].classList.remove("primo_gruppo");
+        div[i].classList.remove("secondo_gruppo");
+        div[i].classList.remove("blu");
+    }
+})
