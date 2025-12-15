dbd90760dfdfb5c9558ac1e85143bc09062b8bad
lisa <lisa.casaburo@outlook.it>
2025-12-15 11:49:46 +0100

    pagina php

---
+++ crossi/esercizi/groups/pagina.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button id="bottonerosso">Rosso</button>
+     <?php
+    for ($i = 1; $i <= 15; $i++){
+        echo("<div>Questo è il div numero $i</div>");
+    }
+    ?>
+    
+</body>
+</html>
