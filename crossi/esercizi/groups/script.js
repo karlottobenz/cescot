21f8bec48d2087c630391620b47fa2baa7bbc32d
karlottobenz <fmsemprini@gmail.com>
2025-12-17 12:10:02 +0100

    Finito

--- crossi/esercizi/groups/script.js
+++ crossi/esercizi/groups/script.js
@@ -16,37 +16,80 @@ var bottoneSecondoGiallo = document.getElementById('secondogiallo');
 var bottoneTerzoGiallo = document.getElementById('terzogiallo');
 var bottoneTuttiGiallo = document.getElementById('tuttigiallo');
 
-
-var PrimoGruppo = document.getElementsByClassName('primo_gruppo');
-var SecondoGruppo = document.getElementsByClassName('secondo_gruppo');
-var TerzoGruppo = document.getElementsByClassName('terzo_gruppo');
-
+/*====================== ROSSO ======================*/
 bottonePrimoRosso.addEventListener('click', function(){
-    for(let i = 0; i <= PrimoGruppo.length; i++){
-        PrimoGruppo[i].classList.add("rosso");
-        PrimoGruppo[i].classList.remove("blu");
-        PrimoGruppo[i].classList.remove("giallo");
-    }
+    change_divs_color("primo_gruppo", "red");
 })
 
 bottoneSecondoRosso.addEventListener('click', function(){
-    for(let i = 0; i <= SecondoGruppo.length; i++){
-        SecondoGruppo[i].classList.remove("blu");
-        SecondoGruppo[i].classList.add("rosso");
-        PrimoGruppo[i].classList.remove("giallo");
-    }
+    change_divs_color("secondo_gruppo", "red");
+})
+
+bottoneTerzoRosso.addEventListener('click', function(){
+    change_divs_color("terzo_gruppo", "red");
 })
 
 bottoneTuttiRosso.addEventListener('click', function(){
-    for(let i = 0; i <= PrimoGruppo.length; i++){
-        PrimoGruppo[i].classList.add("rosso");
-        PrimoGruppo[i].classList.remove("blu");
-        PrimoGruppo[i].classList.remove("giallo");
-    }
+    change_divs_color("primo_gruppo", "red");
 
-    for(let j = 0; j <= SecondoGruppo.length; j++){
-        SecondoGruppo[j].classList.add("rosso");
-        SecondoGruppo[j].classList.remove("blu");
-        PrimoGruppo[i].classList.remove("giallo");
-    }
+    change_divs_color("secondo_gruppo", "red");
+
+    change_divs_color("terzo_gruppo", "red");
 })
+
+
+/*====================== BLU ======================*/
+bottonePrimoBlu.addEventListener('click', function(){
+    change_divs_color("primo_gruppo", "blue");
+})
+
+bottoneSecondoBlu.addEventListener('click', function(){
+    change_divs_color("secondo_gruppo", "blue");
+})
+
+bottoneTerzoBlu.addEventListener('click', function(){
+    change_divs_color("terzo_gruppo", "blue");
+})
+
+bottoneTuttiBlu.addEventListener('click', function(){
+    change_divs_color("primo_gruppo", "blue");
+
+    change_divs_color("secondo_gruppo", "blue");
+
+    change_divs_color("terzo_gruppo", "blue");
+})
+
+
+/*===================== GIALLO =====================*/
+bottonePrimoGiallo.addEventListener('click', function(){
+    change_divs_color("primo_gruppo", "yellow");
+})
+
+bottoneSecondoGiallo.addEventListener('click', function(){
+    change_divs_color("secondo_gruppo", "yellow");
+})
+
+bottoneTerzoGiallo.addEventListener('click', function(){
+    change_divs_color("terzo_gruppo", "yellow");
+})
+
+bottoneTuttiGiallo.addEventListener('click', function(){
+    change_divs_color("primo_gruppo", "yellow");
+
+    change_divs_color("secondo_gruppo", "yellow");
+
+    change_divs_color("terzo_gruppo", "yellow");
+})
+
+
+// Funzioni per cambio colore
+function change_divs_color(div_class, bg_color){
+    let myDivs = document.getElementsByClassName(div_class);
+    change_divs_style(myDivs, bg_color);
+}
+
+function change_divs_style(divs, backgroundColor){
+    for(let i = 0; i < divs.length; i++){
+        divs[i].style.backgroundColor = backgroundColor;
+    }
+}
