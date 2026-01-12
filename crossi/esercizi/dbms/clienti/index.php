e542bc71b219e33938af944a911e56bb8ed3558a
karlottobenz <fmsemprini@gmail.com>
2026-01-12 12:03:26 +0100

    Clienti

---
+++ crossi/esercizi/dbms/clienti/index.php
@@ -0,0 +1,33 @@
+<?php
+    //inizializza la connessione al database
+    $databaseHost = '127.0.0.1';
+    $databaseName = 'cescot';
+    $databaseUsername = 'root';
+    $databasePassword = '';
+?>  
+<html>
+    <head>
+        <title>Esercizio</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <h1>Clienti</h1>
+        <?php
+            $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+            //verifica la connessione
+            if (!$mysqli) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            //esegui una query di esempio
+            $query = 'SELECT * FROM clienti';
+
+            $result = mysqli_query($mysqli, $query);
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div class = 'cliente'>
+                        <h2>$row[id]</h2>
+                        <p>$row[nome] $row[cognome]</p> </div>";
+            }
+        ?>
+</html>
