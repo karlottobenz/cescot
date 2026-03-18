59ff1c0710e08e629541415d20cd3726d8043a45
karlottobenz <fmsemprini@gmail.com>
2026-03-18 11:35:41 +0100

    creato inserimento clienti

---
+++ crossi/esercizi/dbms/prenotazioni/inserimento-clienti.php
@@ -0,0 +1,59 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Inserimento cliente</h1>
+        <?php
+            require_once("../../../php/esercizi/lib/libreria.php");
+            /*inizializza la connessione al database
+            $databaseHost = '127.0.0.1';
+            $databaseName = 'prenotazioni';
+            $databaseUsername = 'root';
+            $databasePassword = '';
+            $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); */
+
+            $mysqli = new mysqli(
+                getenv("DB_HOST"),
+                getenv("DB_USER"),
+                getenv("DB_PASS"),
+                getenv("DB_NAME")
+            );
+
+            //verifica la connessione
+            if (!$mysqli) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            $regioniQuery = "SELECT id_regione, regione FROM regioni ORDER BY regione";
+            $regioniResult = $mysqli->query($regioniQuery);
+
+        ?>
+        <form method="GET">
+            <input type="text" placeholder="Nome" required>
+            <input type="text" placeholder="Cognome" required> <br>
+
+            <label for="regione">Seleziona regione:</label>
+
+            <select name="regione" id="regione" required>
+                <option value="">-- Tutte le regioni --</option>
+
+                <?php
+                while ($reg = $regioniResult->fetch_assoc()) {
+                    $selected = ($regioneSelezionata == $reg['id_regione']) ? "selected" : "";
+                    echo "<option value='{$reg['id_regione']}' $selected>{$reg['regione']}</option>";
+                }
+                ?>
+            </select>
+            <br><br>
+            <input type="submit" value="Salva">
+            <input type="reset" value="Annulla">
+        </form>
+
+        <?php
+            $query = 'ALTER TABLE clienti
+                      INSERT INTO clienti';
+        ?>
+    </body>
+</html>
